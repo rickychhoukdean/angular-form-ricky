2c4f3212655c1be8437f76f9ccd9134a808d0481
rickychhoukdean <chhoukdean184@msn.com>
2020-08-07 17:34:56 -0400

    App with full functionality before refactoring

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -3,7 +3,7 @@ import { Example } from "./example";
 import { HttpService } from "./http.service";
 import { FileUploader } from "ng2-file-upload";
 const uploadAPI =
-  "https://Basic-backend-eA-coding-test.rickychhoukdean.repl.co/api/upload"; 
+  "https://Basic-backend-eA-coding-test.rickychhoukdean.repl.co";
 
 @Component({
   selector: "app-form",
@@ -13,7 +13,7 @@ const uploadAPI =
 export class FormComponent implements OnInit {
   public uploader: FileUploader = new FileUploader({
     url: uploadAPI,
-    itemAlias: "file"
+    itemAlias: "file",
   });
 
   model: Example = new Example();
@@ -23,17 +23,23 @@ export class FormComponent implements OnInit {
   errorMsg: String = "";
 
   onSubmit() {
-    this._httpService.postData(this.model).subscribe(
-      data => {
-        this.submitted = true;
-      },
-      error => (this.errorMsg = `Error submitting form : ${error.statusText}`)
-    );
+    // this._httpService.postData(this.model).subscribe(
+    //   data => {
+    //     this.submitted = true;
+    //   },
+    //   error => (this.errorMsg = `Error submitting form : ${error.statusText}`)
+    // );
 
-
-    
+    console.log(this.model);
   }
   ngOnInit() {
+
+    this.uploader.onBuildItemForm = (fileItem: any, form: any) => {
+      form.append("name",this.model.name); //note comma separating key and value
+      form.append("dob",this.model.dob);
+      form.append("option",this.model.option);
+      form.append("file",this.model.file);
+    };
     this.uploader.onAfterAddingFile = file => {
       file.withCredentials = false;
     };
@@ -42,6 +48,8 @@ export class FormComponent implements OnInit {
       response: any,
       status: any,
       headers: any
-    ) => {};
+    ) => {
+      console.log(item, "item", response, "response");
+    };
   }
 }
